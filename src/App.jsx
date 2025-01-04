50cbd6685b69999586a7da23ff7fa8334dae81f6
SazzadForazi <sazzad.csebubt@gmail.com>
2025-01-05 00:30:17 +0600

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
 import "./App.css"
+import Counter from "./components/Counter";
 
-const App = () => {
-  const people = [{
-    id: 0,
-    name: 'Creola Katherine Johnson',
-    profession: 'mathematician',
-  }, {
-    id: 1,
-    name: 'Mario José Molina-Pasquel Henríquez',
-    profession: 'chemist',
-  }, {
-    id: 2,
-    name: 'Mohammad Abdus Salam',
-    profession: 'physicist',
-  }, {
-    id: 3,
-    name: 'Percy Lavon Julian',
-    profession: 'chemist',  
-  }, {
-    id: 4,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }];
-  const chemists = people.filter((person) => person.profession === 'chemist');
-  console.log(chemists);
+function App() {
   return (
-   <>
-   <ul>
-   {/* {people.map((person) => (
-    <li key={person.id}>{person.name}</li>
-   ))} */}
-
-
-   {chemists.map((chemist) => (
-    <li key={chemist.id}>{chemist.name}</li>
-   ))}
-   </ul>
-   </>
+    <div>
+      <Counter />
+    </div>
   );
-};
+}
 
-export default App; 
+export default App;

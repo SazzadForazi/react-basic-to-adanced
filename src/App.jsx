82ec4c6ae692abe0fc733ca202721cde82295444
SazzadForazi <sazzad.csebubt@gmail.com>
2025-01-05 20:53:25 +0600

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div>
-      <Counter />
+      {/* <Counter /> */}
+      <Todos/>
     </div>
   );
 }
